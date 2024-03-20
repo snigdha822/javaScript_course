5c07c0da966271cd8e31e7277c08dfb04cb65b58
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-03-20 20:09:57 +0530

    Add files via upload

---
+++ Day-3/main.js
@@ -0,0 +1,30 @@
+// console.log('Hallo World!')
+
+
+// for(let i =1;i<=10;i++){
+//     console.log(i);
+// }
+
+// for in loop --->
+// let obj = {
+//     name: "John",
+//     age:30,
+//     city: "USA"
+// }
+
+// for(let abc in obj){
+//     console.log(abc);
+// }
+
+// let greet = "Welcome";
+
+// for(let abc of greet){
+//     console.log(abc);
+// }
+
+//While loop --->
+let i = 0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
