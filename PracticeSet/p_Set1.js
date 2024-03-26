f4d4a047f35058bd94603eb177f2cec98ce7d7f1
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-03-26 18:16:31 +0530

    Add files via upload

---
+++ PracticeSet/p_Set1.js
@@ -0,0 +1,13 @@
+const bazarItems = ["potato","onion","tomato","coriender","paneer"];
+
+// modify an item --->
+bazarItems[1] = "garlic"; 
+
+console.log(bazarItems.length);
+console.log(bazarItems[1]);
+console.log(bazarItems[4]);
+console.log(bazarItems[0]);
+
+// const random = ["tree", 795, [0, 10, 20]];
+// console.log(random[2][1]);
+
