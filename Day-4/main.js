6a91527459e3dd1676de58b669caa83fa7f60922
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-04-14 16:40:23 +0530

    Add files via upload

---
+++ Day-4/main.js
@@ -0,0 +1,36 @@
+// What is function?
+// A function is a block of code that can be called and executed by any time.
+
+function callMyName()
+{
+    console.log("My name is Snigdha")
+}
+callMyName();
+
+
+
+
+//What is parameter and argument?
+
+// function addTwoNumbers(num1,num2)
+// {
+//     return num1+num2;
+// }
+
+// let sum = addTwoNumbers(50,100);
+// console.log(sum);
+
+
+
+//Global Scope ( var ) ----->
+// function Greet()
+// {
+//     let name = "Piya";
+//     return "Hello World";
+// }
+
+// let x = Greet();
+// console.log(x);
+
+//Local Scope ( let, const ) ----->
+
