7629558492ab95c85fcfc0c3ba0672060d213337
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-04-15 07:44:40 +0530

    Add files via upload

---
+++ Day-5/main.js
@@ -0,0 +1,21 @@
+let marks = [90,"70",null,true,{
+    subject:"maths",
+    marks: 89
+}];
+
+marks[5] = -10;
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+// console.log(marks[5]);
+// console.log("The type of our array is: ",typeof marks);
+// console.log("The length of the array is:", marks.length);
+
+// Exercise: print all the values of the arrays
+let student_marks =[90,80,70,60,50,40,30]
+for(let i = 0;i<student_marks.length;i++){
+    console.log(student_marks[i])
+}
