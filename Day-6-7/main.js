9defe1678d351ef06498604360517992fc5da13a
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-04-15 07:47:08 +0530

    Add files via upload

---
+++ Day-6-7/main.js
@@ -0,0 +1,82 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbers, typeof numbers);
+
+// let r = numbers.toString();
+// console.log(r, typeof r);
+
+
+//JOIN() ---->
+// let r = numbers.join("-");
+// console.log(r, typeof r);
+
+// PUSH() ---->
+// let r = numbers.push(10,23,3);
+// console.log(numbers);
+// console.log(r, typeof r);
+
+
+// POP() ---->
+// let r = numbers.pop(); //removes from end
+// console.log(numbers);
+// console.log(r);
+
+
+//Shift() ---->
+// let r = numbers.shift(); // removes from start
+// console.log(numbers);
+// console.log(r);
+
+
+// Unshift() ---->
+// let r = numbers.unshift(0); // Adds from start
+// console.log(numbers);
+// console.log(r);
+
+
+// Concat() ---->
+// let name = ["cat","rat","fat"];
+// let days = ["monday","tuesday","friday"];
+// let newArray = numbers.concat(name,days);
+// console.log(newArray);
+
+
+// Sort() ----->
+// let newNum = [12,30,54,0,5];
+// newNum.sort();
+// console.log(newNum);
+
+
+// forEach() ---->
+// let r = numbers.forEach((value,index,array)=>{
+//     console.log(value,index,array)
+    // console.log(r*value)
+// });
+
+// console.log(r)
+
+
+// map() ---->
+// let r = numbers.map((value)=>{
+//     //console.log(value,index,array)
+//     return value*r
+// })
+
+// console.log(r);
+
+
+// filter() ---->
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// let greaterTHAN3 = numbers.filter((value,index,array)=>{
+//     return value >= 3;
+// });
+
+// console.log(greaterTHAN3);
+
+
+// redus() ---->let adds = numbers.reduce((prev,curv)=>{
+//     // console.log(prev,curv)
+//     return prev+curv;
+//  })
+//   console.log(adds);
+//  
