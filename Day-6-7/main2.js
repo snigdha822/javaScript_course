a046f845f98ed0883ab6eb51f6b0248115c71718
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-05-04 13:31:54 +0530

    Add files via upload

---
+++ Day-6-7/main2.js
@@ -0,0 +1,53 @@
+//* 1 Length- Property
+// let sampleValue = "Hello World!";
+// console.log(sampleValue.length);
+
+// // *2. ToUpperCase()
+// let name = "my name is piya";
+// let nm = name.toUpperCase()
+// console.log(nm);
+
+// // *3. ToLowerCase()
+// let name1 = "MY NAME IS PIYA";
+// let nm1 = name.toLowerCase();
+// console.log(nm1);
+
+// *4. charAt()  //Returns the character at the specified index in a string.
+// let str = "Hello World!";
+// let res = str.charAt(9);
+// console.log(res);
+
+// *5. indexOf()
+// let str1 = "Hello World!";
+// let res1 = str1.indexOf("l");
+// console.log(res1);
+
+// *6. lastIndexOf()
+// let str2 = "Hello World!";
+// let res2 = str2.lastIndexOf("H");
+// console.log(res2);
+
+// *7. includes()
+// let str3 = "Hello World!";
+// let res3 = str3.includes("World");  // Included is true
+// console.log(res3);
+
+// *8. replace()
+// let str4 = "Hello World";
+// let res4 = str4.replace("World" , "Programmers👋");
+// console.log(res4);
+
+// *9. concat()
+// let str5 = "Hello";
+// let res5 = str5.concat(" World!");
+// console.log(res5);
+
+// *10. slice()
+// let str6 = "Hello World!";
+// let res6 = str6.slice(3,12);
+// console.log(res6);
+
+// *11. substr:
+let str7 = "Hello World!";
+let res7 = str7.substr(6,6);
+console.log(res7);
