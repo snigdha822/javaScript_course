a93f4f6e2f8fe835b87f82d2f084d208abf8100e
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-05-05 20:10:45 +0530

    Add files via upload

---
+++ Day-6-7/main3.js
@@ -0,0 +1,42 @@
+// for loop ---->
+// let number = 1;
+
+// for ( let i = 1; i <= 10; i++){
+//     console.log( number+i );
+// }
+
+// for-in loop ---->
+//  let obj = {
+//     name: "Snigdha",
+//     role: "Programmer",
+//     company: "Google"
+//  }
+
+//  for(let key in obj) {
+//     let element = obj[key];
+//     console.log(element);
+//     //console.log(key);
+//     //console.log(key, element);
+//  }
+
+
+ // for-of loop ---->
+//  for (const c of "Google") {
+//     console.log(c);
+//  }
+
+// While loop ---->
+// let i = 5;
+// while (i < 10) {
+//     console.log(i);
+//     i ++;
+// }
+
+
+// Do-While loop ---->
+ let i = 0 ;
+do {
+    console.log(i);
+    i ++;
+}
+ while (i < 10);
