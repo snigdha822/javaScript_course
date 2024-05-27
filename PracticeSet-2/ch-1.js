eab53f215e44130429f2ae1d579718e281ecb276
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-05-27 22:06:22 +0530

    Add files via upload

---
+++ PracticeSet-2/ch-1.js
@@ -0,0 +1,22 @@
+// let a = "Ram";
+// let b = 6;
+// let r = a+b;
+// console.log(r, typeof r);
+
+// const a = {
+//     name: "Piya",
+//     age: 20,
+//     isStudent: true
+// }
+// a['name2'] = "Snigdha"
+// a['name'] = "Ram"
+// console.log(a);
+
+const dict = {
+    yakka: "hard work",
+    appreciate: "recognize the full worth of",
+    hello: "is a greeding style in english"
+}
+
+console.log(dict.hello);
+
