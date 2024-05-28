013af62826c70102a9bd35e87d2c3a1aa749633f
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-05-28 21:48:15 +0530

    Add files via upload

---
+++ PracticeSet-2/ch-2.js
@@ -0,0 +1,51 @@
+// Operators and Expressions
+
+// let a = 15;
+// let b = 4;
+
+// Arithmatic Operators
+// console.log ( "a+b = ", a+b);
+// console.log ( "a/b = ", a-b);
+// console.log ( "a-b = ", a/b);
+// console.log ( "a*b = ", a*b);  // 15*15*15*15
+// console.log ( "a**b = ", a**b);
+// console.log ( "a++ = ", a++);
+// console.log ( "a = ", a);
+// console.log ( "++a = ", ++a);
+// console.log ( "a-- = ", a--);
+// console.log ( "a = ", a);
+
+// Assignment Operators
+
+// let c = 10;
+// c += 5;
+// console.log(c);
+
+// Comparison Operators
+
+// let comp1 = 10;
+// let comp2 = "10";
+
+// console.log(" comp1 == comp2 is ", comp1 == comp2);
+// console.log(" comp1 != comp2 is ", comp1 != comp2);
+// console.log(" comp1 === comp2 is ", comp1 === comp2);
+// console.log(" comp1 !== comp2 is ", comp1 !== comp2);
+
+// Logical Operators
+// let x = 5;
+// let y = 6;
+// console.log( x<y && x==5);
+// console.log( x>y || y==6);
+// console.log( !false);
+// console.log( !true);
+
+// Conditional Opperators
+
+let age = 17;
+age = Number.parseInt(age); //convert a string into number
+console.log(typeof age)
+if (age >=18){
+    console.log(" You are an adult")}
+    else{
+        console.log(" You are not an adult")
+    }
