eacdcd8a915941940f07ff3dd0f357e5b1ad5b21
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-05-31 22:21:52 +0530

    Add files via upload

---
+++ PracticeSet-2/ch-3.js
@@ -0,0 +1,29 @@
+// let marks = {
+//     Ajay: 95,
+//     Sujay: 86,
+//     Ram: 89
+// }
+
+// for (let i = 0; i < Object.keys(marks).length; i++){
+//     console.log ("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+// }
+
+// For in loop ---> easier than for loop
+// for (let key in marks){
+//     console.log ("The marks of " + key + " are " + marks[key])
+// }
+
+// const mean = (a,b,c,d,e)=>{
+//     return (a+b+c+d+e)/5;
+// }
+
+// console.log("Mean of 2,3,4,5,6 are", mean(2,3,4,5,6))
+
+// let cn = 10;
+// let i ;
+// while(i != cn){
+//     i = prompt("Enter a number: ")
+//     console.log("Opps! try again")
+// }
+// console.log("You have entered the correct number")
+
