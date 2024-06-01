504bcaf25f427a3dcb47fc7e9c9d872744df5ff8
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-06-01 23:44:15 +0530

    Add files via upload

---
+++ PracticeSet-2/ch-4.js
@@ -0,0 +1,35 @@
+let marks = [90,98,78,80,false,"Not bad"]
+// console.log(marks)
+// console.log(marks[2])
+// console.log(marks[0])
+// console.log(marks[6]) // index 6 does not exist
+// console.log(" The length of the array is:",marks.length)
+// marks[3] = 97 // changing a value of an array
+// marks[6] = 95 // adding a value of an array
+// console.log(marks)
+// console.log(typeof marks)
+
+// //prints all elements using for loop
+// for (let i = 0; i< marks.length; i++){
+//     console.log(marks[i])
+// }
+
+// Array methods --->
+let num = [1,2,3,4,5]
+let a = num.toString() // a is now string
+// console.log(a, typeof a)
+// let c = num.join("-")
+// console.log(c)
+
+// num.pop()
+// console.log(num)
+
+// num.push(13)
+// console.log(num)
+
+// num.shift()
+// console.log(num)
+
+num.unshift(50)
+console.log(num)
+
