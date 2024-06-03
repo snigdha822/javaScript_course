9dc3b99710b61e59154f9eac25cb05890f186ebc
Snigdha_M <134606049+snigdha822@users.noreply.github.com>
2024-06-03 23:54:55 +0530

    Add files via upload

---
+++ PracticeSet-2/ch-4(LoopsArray).js
@@ -0,0 +1,21 @@
+let  num = [3,45,6,7,89]
+
+// ForEach Loop
+// num.forEach((element) => {
+//     console.log(element*element)
+// })
+
+// Array From
+let name = "Piya"
+// let arr = Array.from(name)
+// console.log(arr)
+
+// For..Of Loop
+for (let i of num){  //instead of ForEach, we can use this
+    console.log(i)
+}
+
+// For..In Loop
+for (let item in num){
+    console.log(item)
+}
